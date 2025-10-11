76ad5915c53bb962f028e5f2f2006fd442419a7b
Bakari Levy <bakarilevy@gmail.com>
2025-10-11 16:35:26 -0400

    Initial Commit

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/messages.route.js";
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+app.listen(PORT, () => console.log("Server running on port 3000"));
+
