34aeff1b1316a2e5ffc130bbe93ae809a28afcc3
Bakari Levy <bakarilevy@gmail.com>
2025-10-12 19:27:14 -0400

    Welcome email added and refactoring

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,8 +1,9 @@
 import jwt from "jsonwebtoken";
+import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
 
-    const {JWT_SECRET} = process.env;
+    const {JWT_SECRET} = ENV;
     if(!JWT_SECRET) {
         throw new Error("JWT_SECRET is not configured");
     }
@@ -15,7 +16,7 @@ export const generateToken = (userId, res) => {
         maxAge: 7 * 24 * 60 *60 * 1000, // 7 Days in Milliseconds
         httpOnly: true, // Prevent XSS
         sameSite: "strict", // Prevent CSRF
-        secure: process.env.NODE_ENV === "development" ? false : true,
+        secure: ENV.NODE_ENV === "development" ? false : true,
     });
 
     return token;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,14 @@
 import path from "path";
 import express from "express";
-import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/messages.route.js";
 import { connectDB } from "./lib/db.js";
-
-dotenv.config();
+import {ENV} from "./lib/env.js";
 
 const app = express();
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 3000;
+const PORT = ENV.PORT || 3000;
 
 app.use(express.json());
 
@@ -18,7 +16,7 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/messages", messageRoutes);
 
 // Production script
-if(process.env.NODE_ENV === "production") {
+if(ENV.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get("*", (_, res) => {
