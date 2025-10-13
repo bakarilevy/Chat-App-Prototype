619f98dc26766e6f2e2f8e5e935444d2dd1b0e1c
Bakari Levy <bakarilevy@gmail.com>
2025-10-13 16:47:21 -0400

    Added ratelimiting using Arcjet

---
+++ backend/src/middleware/arcjet.middleware.js
@@ -0,0 +1,33 @@
+import aj from "../lib/arcjet.js";
+import {isSpoofedBot} from "@arcjet/inspect";
+
+
+export const arcjetProtection = async (req,res,next) => {
+    try {
+        const decision = await aj.protect(req);
+        if(decision.isDenied()) {
+            if(decision.reason.isRateLimit()) {
+                return res.status(429).json({message: "Rate limit exceeded. Please try again later."});
+            }
+            else if (decision.reason.isBot()) {
+                return res.status(403).json({message: "Bot access denied"});
+            } else {
+                return res.status(403).json({message: "Access denied by security policy"});
+            }
+        }
+
+        // Check for spoofed bots.
+        if(decision.results.some(isSpoofedBot)) {
+            // TODO: Log IP of address and add to Blacklist
+            return res.status(403).json({
+                error: "Spoofed bot detected.",
+                message: "Malicious bot activity detected."
+            });
+        }
+        
+        next();
+    } catch(error) {
+        console.error("Arcjet Protection error: ", error);
+        next();
+    }
+}
