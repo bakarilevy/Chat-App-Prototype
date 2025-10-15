a8d87e18a392eca898a673ca1ee2482b1946e2dc
Bakari Levy <bakarilevy@gmail.com>
2025-10-15 18:29:10 -0400

    Components for chat updated

--- frontend/src/components/ProfileHeader.jsx
+++ frontend/src/components/ProfileHeader.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import {useState, useRef} from "react";
+import {LogOutIcon, VolumeOffIcon, Volume2Icon } from "lucide-react";
+import {useAuthStore} from "../store/useAuthStore";
+import {useChatStore} from "../store/useChatStore";
 
 function ProfileHeader() {
+
+  const {logout, authUser, updateProfile} = useAuthStore();
+  const {isSoundEnabled, toggleSound} = useChatStore();
+  const [selectedImg, setSelectedImg] = useState(null);
+
+  const fileInputRef = useRef(null);
+  
+  const handleImageUpload = (e) => {}
+
   return (
-    <div>
-      
+    <div className="p-6 border-b border-slate-700/50">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          {/* AVATAR */}
+          
+        </div>
+      </div>
     </div>
   )
 }
